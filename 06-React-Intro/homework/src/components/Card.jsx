60e614267a758ab9066430e29e40a13308cb1abb
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-10-03 20:34:36 -0300

    06-React-Intro resuelto

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,14 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return <div>
+    <button onClick={props.onClose}>X</button>
+    <h1>{props.name}</h1>
+    <div>
+      <h4>Min - {props.min}</h4>
+      <h4>Max - {props.max}</h4>
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="" />
+    </div>
+  </div>
 };
+
--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import Card from './Card';
+
 
 export default function Cards(props) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  if(props.cities){
+    return ( 
+      <div>
+        {
+        props.cities && props.cities.map(city=>(
+            <Card
+              key={city.id}
+              max={city.main.temp_max}
+              min={city.main.temp_min}
+              name={city.name}
+              img={city.weather[0].icon}
+              onClose={()=> alert(city.name)}
+            />
+          ))
+        }
+      </div>
+    )
+  }else{
+    return <h4>No hay ciudades disponible</h4>
+  }
+  
 };
