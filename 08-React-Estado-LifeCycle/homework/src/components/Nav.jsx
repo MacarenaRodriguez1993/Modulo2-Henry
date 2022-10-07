6ed8ce64ac024c67ecebc4f820739858bd7adff6
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-10-07 12:04:20 -0300

    08-React-Estado-LifeCycle resuelto

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import Logo from '../logoHenry.png'
+import Logo from '../logoHenry.png';
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
 
 function Nav({onSearch}) {
   return (
-    <div>Nav</div>
+    <nav className='navbar navbar-expand-lg bg-dark'>
+      <div className='container-fluid'>
+        <a  className='navbar-brand nav-link' href="#">
+          <img src={Logo} alt="img not found" />
+          <span> Henry weather app</span>
+        </a>
+        <SearchBar onSearch={onSearch}/>
+      </div>
+    </nav>
   );
 };
 
--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 
 export default function SearchBar({onSearch}) {
+
+  const [city,setCity]=useState('');
+
   return (
-    <form onSubmit={(e) => {
+    <form className="d-flex"onSubmit={(e) => {
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
     }}>
       <input
+        className="form-control me-2"
         type="text"
         placeholder="Ciudad..."
+        value={city}
+        onChange={e=>{
+          setCity(e.target.value)
+        }}
       />
-      <input type="submit" value="Agregar" />
+      <button className="btn btn-outline-success" type="submit">Agregar</button>
     </form>
   );
 }
